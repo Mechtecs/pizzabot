d8a0ff84213874c73a499814770796ccbb316e62
Max Schorradt <schorradt@publicplan.de>
2016-06-25 00:03:14 +0200

    mehr

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
-var express = require('express');
-var app = express();
+var restify = require('restify');
+var builder = require('botbuilder');
+var http = require('superagent');
+var recast = require('recastai')
 
-app.set('port', (process.env.PORT || 5000));
+// init Microsoft bot connector
+var bot = new builder.BotConnectorBot({ appId: process.env.BOTCONNECTOR_APPID, appSecret: process.env.BOTCONNECTOR_APPSECRET });
+// process.env.BOTCONNECTOR_APPID
 
-app.use(express.static(__dirname + '/public'));
+// Require all my intents file
+var Iorder_food = require('./modules/order_food');
+var Itopping = require('./modules/topping');
+var Isize = require('./modules/size');
+var Ilocation = require('./modules/location');
+var Itime = require('./modules/time');
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var INTENTS = {
+  order_food: Iorder_food,
+  topping: Itopping,
+  size: Isize,
+  location: Ilocation,
+  time: Itime,
+}
+var CLIENT = new recast.Client(process.env.RECAST_REQUESTTOKEN);
+// Create bot and add dialogs
+bot.add('/', function (session) {
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+  var input = session.message.text.toString().replace(/\0/g, '');
+
+  CLIENT.textRequest(input, function (res, err) {
+    if (err) {
+      session.send('Das habe ich nicht verstanden.');
+    } else {
+      var intent = res.intent();
+
+      if (intent && INTENTS[intent]) {
+        INTENTS[intent](res)
+          .then(function (res) { session.send(res) })
+          .catch(function (res) { session.send(res) });
+      } else {
+        session.send('Entschuldigung, ich konnte nicht verstehen, was Sie von mir wollen. Können Sie es erneut versuchen?');
+      }
+    }
+  })
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+  console.log('%s listening to %s', server_host, server_port);
 });
-
-
---
+++ modules/location.js
@@ -0,0 +1,20 @@
+function location(response) {
+    var location = null;
+    var datetime = null;
+    var duration = null;
+
+    result = response.sentence();
+    location = response.get('location');
+
+    if (location) {
+      return new Promise(function (resolve, reject) {
+          resolve('Sie wollen eine Waffenbesitzkarte in ' + location.raw + ' beantragen. Sekunde, ich schaue nach der zuständigen Stelle.');
+      });
+    } else {
+      return new Promise(function (resolve, reject) {
+          reject('Entschuldigung, mir fehlt der Ort zu Ihrer Frage, um eine eindeutige Zuständigkeit ermitteln zu können!');
+      });
+    }
+}
+
+module.exports = location;
